1054667b8e8d307c4fa31f459c18d4f689b0ad2b
shin1x1 <shin1x1@gmail.com>
2021-11-13 16:12:12 +0900

    Return a error exit code if parse error has occured

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,7 @@
 import lexer.Lexer
 import parser.Parser
 import stream.CharacterStream
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
     System.`in`.use {
@@ -8,5 +9,7 @@ fun main(args: Array<String>) {
         val json = Parser(lexer).parse()
 
         println(json)
+
+        json.onFailure { exitProcess(1) }
     }
 }

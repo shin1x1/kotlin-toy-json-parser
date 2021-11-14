d07d4be28bd8120a7aad10afb0fa01ed88a4f14c
shin1x1 <shin1x1@gmail.com>
2021-11-14 12:56:09 +0900

    Add json file support

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,16 @@
 import lexer.Lexer
 import parser.Parser
 import stream.CharacterStream
+import java.io.File
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-    System.`in`.use {
+    val input = when(args.size) {
+        0 -> System.`in`
+        else -> File(args[0]).inputStream()
+    }
+
+    input.use {
         val lexer = Lexer(CharacterStream(it))
         val json = Parser(lexer).parse()
 
